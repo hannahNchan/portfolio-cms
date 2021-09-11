102cb7724da10b4522c730f3b81bb7e169f1573f
Hannah <hannah.n.kahnwald@gmail.com>
2021-09-10 19:55:26 -0500

    set environment varaibles

--- src/api/index.js
+++ src/api/index.js
@@ -1,38 +1,41 @@
-const api_dev = process.env.REACT_APP_API_ENDPOINT_DEV;
+const api =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_ENDPOINT_DEV
+    : process.env.REACT_APP_API_ENDPOINT_PROD;
 
 export const _POST = async (payload, id) => {
-  const formedURL = `${api_dev}/set-career/v1/${id}`;
+  const formedURL = `${api}/set-career/v1/${id}`;
   const options = {
     method: "POST",
     body: JSON.stringify(payload),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
   const response = await fetch(formedURL, options);
   return await response.json();
-}
+};
 
 export const _POST_CREATE_NEW = async (payload) => {
-  const formedURL = `${api_dev}/new-set-career/v1`;
+  const formedURL = `${api}/new-set-career/v1`;
   const options = {
     method: "POST",
     body: JSON.stringify(payload),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
   const response = await fetch(formedURL, options);
   return await response.json();
-}
+};
 
 export const _DELETE = async (id) => {
-  const formedURL = `${api_dev}/delete-set-career/v1/${id}`;
+  const formedURL = `${api}/delete-set-career/v1/${id}`;
   const options = {
     method: "GET",
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
   const response = await fetch(formedURL, options);
   return await response.json();
